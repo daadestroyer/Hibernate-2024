d8d3502424c82e14b560c80dbcc26a8a28618604
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-06-26 12:32:07 +0530

    first level cache program added...

--- src/main/java/com/thecoderstv/hibernate/caching/firstlevelcache/App.java
+++ src/main/java/com/thecoderstv/hibernate/caching/firstlevelcache/App.java
@@ -15,17 +15,29 @@ public class App {
 		worker.setId(101);
 		worker.setName("Ram Singh");
 		
-//		ssn.save(worker);
+		//ssn.save(worker);
+		//txn.commit();
 		
 		Worker savedWorker1 = ssn.get(Worker.class, 101);
 		System.out.println(savedWorker1);
-		
+
 		// working something
 		
-// 		ssn.close(); // but if you close the cache you will not be able to fetch again
+		ssn.close(); // but if you close the cache you will not be able to fetch again
+		
+
+		if(ssn.contains(savedWorker1)) {
+			System.out.println("worker object is present in first level cache");
+		}
+		else {
+			System.out.println("worker object is not present in first level cache");
+		}
+
 		// here you can notice select query run only one time
 		Worker savedWorker2 = ssn.get(Worker.class, 101);
 		System.out.println(savedWorker2);
+		
+		
 		
 		
 	}
