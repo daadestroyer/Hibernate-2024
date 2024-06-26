1da0635ca40af78dfbb9aadf0320abbe99e09ff1
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-06-26 12:58:57 +0530

    first level cache program added...

--- src/main/java/com/thecoderstv/hibernate/caching/secondlevelcache/Worker.java
+++ src/main/java/com/thecoderstv/hibernate/caching/secondlevelcache/Worker.java
@@ -1,40 +1,40 @@
-//package com.thecoderstv.hibernate.caching.secondlevelcache;
-//
-//import org.hibernate.annotations.Cache;
-//import org.hibernate.annotations.CacheConcurrencyStrategy;
-//
-//import jakarta.persistence.Cacheable;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//
-//@Entity
-//@Cacheable
-//@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
-//public class Worker {
-//	@Id
-//	private int id;
-//	
-//	private String name;
-//
-//	public int getId() {
-//		return id;
-//	}
-//
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Worker [id=" + id + ", name=" + name + "]";
-//	}
-//	
-//}
+package com.thecoderstv.hibernate.caching.secondlevelcache;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Worker {
+	@Id
+	private int id;
+	
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Worker [id=" + id + ", name=" + name + "]";
+	}
+	
+}
